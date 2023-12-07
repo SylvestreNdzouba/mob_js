74b65f6d4937a2b1396c0cd483be34405c56bb15
Olivia LINSTRUMELLE <olinstru@gmail.com>
2023-12-07 16:07:39 +0100

    this works

--- main.js
+++ main.js
@@ -3,20 +3,27 @@ const urlApi = "https://cataas.com/cat/";
 async function randomCat() {
   const response = await fetch("https://cataas.com/cat?json=true");
   const data = await response.json();
-
   console.log(data);
   return data._id;
 }
+
 function createURL(id) {
   return urlApi + id;
 }
 
-randomCat();
-const id1 = await randomCat();
-const id2 = await randomCat();
-const id3 = await randomCat();
+let catIds = [];
 
-const element = document.createElement("img");
-element.setAttribute("src", createURL(id1));
-const container = document.getElementById("container");
-container.appendChild(element);
+for (let index = 0; index < 3; index++) {
+  catIds.push(randomCat());
+}
+catIds = await Promise.all(catIds);
+
+catIds.forEach(catId => {
+  const element = document.createElement("img");
+  element.setAttribute("src", createURL(catId));
+  const container = document.getElementById("container");
+  container.appendChild(element);
+});
+
+
+
