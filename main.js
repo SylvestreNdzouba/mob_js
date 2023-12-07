3ec454085cec621c689a625fa469545ad17c4e9b
Olivia LINSTRUMELLE <olinstru@gmail.com>
2023-12-07 16:59:33 +0100

    merci Fabio......

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-const urlApi = "https://cataas.com/cat/";
+const prenoms = [
+  "Fabio",
+  "Romain",
+  "Sylvestre",
+  "Yaakov",
+  "Louis",
+  "Olivia",
+  "Georgina",
+  "Clément",
+  "Cléfranchise",
+]
+
+const urlApi = "https://cataas.com/cat"
+//says/text
 
 async function randomCat() {
-  const response = await fetch("https://cataas.com/cat?json=true");
-  return urlApi + (await response.json())._id;
+  const response = await fetch(`${urlApi}?json=true`)
+  return `${urlApi}/${(await response.json())._id}`
 }
 
-let catUrlsPromises = [];
+let catUrlsPromises = []
 
-for (let index = 0; index < 3; index++) {
-  catUrlsPromises.push(randomCat());
+for (let index = 0; index < prenoms.length; index++) {
+  catUrlsPromises.push(randomCat())
 }
-const catUrls = await Promise.all(catUrlsPromises);
+const catUrls = await Promise.all(catUrlsPromises)
 
-catUrls.forEach(catUrl => {
-  const element = document.createElement("img");
-  element.setAttribute("src", catUrl);
-  const container = document.getElementById("container");
-  container.appendChild(element);
-});
-
-
-
+prenoms.forEach((prenom, pos) => {
+  catUrls[pos] = catUrls[pos] + `/says/${prenom}?fontColor=white`
+  const element = document.createElement("img")
+  element.setAttribute("src", catUrls[pos])
+  const container = document.getElementById("container")
+  container.appendChild(element)
+})
